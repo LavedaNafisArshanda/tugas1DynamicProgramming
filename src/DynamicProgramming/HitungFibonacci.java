4f16d15e43e3a49d73437a8f7abbef509ad26f83
WINDOWS 10 <WINDOWS 10@DESKTOP-BPSCJHN>
2018-09-12 13:38:26 +0700

    Tambah Input

--- src/DynamicProgramming/HitungFibonacci.java
+++ src/DynamicProgramming/HitungFibonacci.java
@@ -9,14 +9,14 @@ private static void tampilJudul(String identitas)
     System.out.println("\nHitung Fibonacci");
     System.out.println("1, 1, 2, 3, 5, 0, 13, 21, ... dst.\n");
 }
-public static void main(String[] args)
-{
+private static int tampilInput() {
+    Scanner arsha = new Scanner(System.in);
     
-
-String identitas = "Laveda Nafis Arshanda / X RPL 3 / 19";
-
-tampilJudul(identitas);
-}
+    System.out.println("Bilangan ke-: ");
+    
+    int n = arsha.nextInt();
+    return n;
+    }
 }
 
 
