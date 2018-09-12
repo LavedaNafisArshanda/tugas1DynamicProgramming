826968eeadfc72eb1b779fa49bf07793c8b09f4f
WINDOWS 10 <WINDOWS 10@DESKTOP-BPSCJHN>
2018-09-12 13:01:22 +0700

    Initial Comit

---
+++ src/DynamicProgramming/HitungFibonacci.java
@@ -0,0 +1,48 @@
+package DynamicProgramming;
+import java.math.BigInteger;
+import java.util.Scanner;
+public class HitungFibonacci {
+private static void tampilJudul(String identitas)
+{
+    System.out.println("Identitias : " + identitas);
+    
+    System.out.println("\nHitung Fibonacci");
+    System.out.println("1, 1, 2, 3, 5, 0, 13, 21, ... dst.\n");
+}
+    private static int tampilInput() {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Bilangan ke-:" );
+        int n = scanner.nextInt();
+        
+        return n;
+    }
+    
+    int n = tampilInput();
+    
+    private static BigInteger fibo(int n) {
+        
+        BigInteger[] hasil = new BigInteger[n];
+        
+        hasil[0] = BigInteger.ONE;
+        hasil[1] = BigInteger.ONE;
+        
+        for (int i = 2; i < n; i++) {
+            hasil[i] = hasil[i-1].add(hasil[i-2]);
+         }
+        
+        return hasil[n-1];
+        
+        
+    }
+    
+    public static void main(String[] args) {
+        
+   
+    String identitas = "Laveda Nafis Arshanda / X RPL 3 / 19";
+    tampilJudul(identitas);
+        
+    }
+    
+
+}
