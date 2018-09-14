de8b747b7c279df9a45a3d662b8a2649e87854cf
WINDOWS 10 <WINDOWS 10@DESKTOP-BPSCJHN>
2018-09-14 18:44:11 +0700

    Biodata

---
+++ src/DynamicProgramming/Tugas01.java
@@ -0,0 +1,15 @@
+
+package DynamicProgramming;
+import java.util.Scanner;
+public class Tugas01 {
+    public static void main(String[] args) {
+        Scanner arsha = new Scanner(System.in);
+        
+        System.out.println("Laveda Nafis Arshanda - X RPL 3 - 19");
+        
+        
+        
+        
+    }
+    
+}
